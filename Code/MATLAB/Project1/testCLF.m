5af2b96abc57b0ab35a3db9d56595249c28f41ce
andrea5972 <andrealindamurphy@gmail.com>
2020-03-12 20:45:04 -0400

    edit file locations

--- Code/MATLAB/Project1/testCLF.m
+++
@@ -1,7 +0,0 @@
-% Project Hand Recognition
-
-% Authors: Andrea Murphy
-% Date: March 31, 2019
-% DESC: American Sign Language Detection
-
-confMatrix = evaluate(categoryClassifier, testSets);
--- Code/MATLAB/Project1/trainCLF.m
+++
@@ -1,9 +0,0 @@
-% Project Hand Recognition
-
-% Authors: Andrea Murphy
-% Date: March 31, 2019
-% DESC: American Sign Language Detection
-
-categoryClassifier = trainImageCategoryClassifier(trainingSets, bag);
-
-confMatrix = evaluate(categoryClassifier, trainingSets);
